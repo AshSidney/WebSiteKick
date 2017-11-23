2f963f53c382440fd933aaeed5618b7dce5d47a6
Zdeno Ash Mikláš <ashsidney@gmail.com>
2017-11-23 14:05:39 +0100

    working on database and file access

--- Kick.php
+++ Kick.php
@@ -1,5 +1,56 @@
 <?php
 
+class DBManager
+{
+  function __construct($database, $server, $user, $password)
+  {
+    $this->dbName = $database;
+    $this->serverName = $server;
+    $this->userName = $user;
+    $this->password = $password;
+  }
+  
+  protected $connection = NULL;
+  
+  function getConnection ()
+  {
+    if ($this->connection == NULL)
+    {
+      try
+      {
+        $this->connection = new PDO("mysql:host=" . $this->serverName
+          . ";dbname=" . $this->dbName . ";charset=utf8",
+          $this->userName, $this->password);
+      }
+      catch (PDOException $e)
+      {
+        echo 'Connection failed: ' . $e->getMessage();
+      }
+    }
+    return $this->connection;
+  }
+  
+  function query ($sqlComm)
+  {
+    return $this->getConnection()->query($sqlComm);
+  }
+  
+  function statement ($sqlComm)
+  {
+    return $this->getConnection()->prepare($sqlComm);
+  }
+}
+
+
+class WPComment
+{
+  void __construct ($db)
+  {
+    $this->db = $db;
+  }
+}
+
+
 class Kicker
 {
   void __construct ()
@@ -7,11 +58,17 @@ class Kicker
     $this->tasks = array(
     require('kick.ini');
       );
+    
+    $this->data = file_exist($this->confFile) ? simplexml_load_file($this->confFile)
+      : new SimpleXMLElement("<?xml version='1.0' standalone='yes'?>");
+    
+    for ($this->tasks as $task)
+      $task->load($this->data);
   }
   
-
   private $tasks;
   private $data;
+  private $confFile = "kick.xml";
 }
 
 ?>
